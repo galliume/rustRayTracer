d3f4338f7b73752a16a412eabcd7f084c34d951e
galliume <gcconantc@gmail.com>
2021-04-26 18:35:30 +0200

    Simplification

--- src/RrtSphere.rs
+++ src/RrtSphere.rs
@@ -5,14 +5,14 @@ use crate::RrtRay::Ray;
 pub fn hit_sphere(center : Vec3, radius : f64, ray : Ray) -> f64 {
     
     let oc : Vec3 = ray.origin() - center;
-    let a : f64 = RrtVec3::dot(ray.direction(), ray.direction());
-    let b : f64 = 2.0 * RrtVec3::dot(oc, ray.direction());
-    let c : f64 = RrtVec3::dot(oc, oc) - radius*radius;
-    let discriminant : f64 = b * b - 4.0 * a * c;
+    let a : f64 = ray.direction().length_squared();
+    let half_b : f64 = RrtVec3::dot(oc, ray.direction());
+    let c : f64 = oc.length_squared() - radius*radius;
+    let discriminant : f64 = half_b * half_b - a * c;
 
     if 0.0 > discriminant {
         return -1.0;
     } else {
-        return (-b - discriminant.sqrt()) / (2.0 * a);
+        return (-half_b - discriminant.sqrt()) / a;
     }
 }
